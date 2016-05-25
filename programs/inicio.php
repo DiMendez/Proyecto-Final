26434a7f30004a15c5d00c188dfa5adf28c6e240
Kevin Clyde Moreno Mc.Manus <Kevin Clyde Moreno Mc.Manus>
2016-05-25 14:34:49 -0500

    inicio.php ya tiene comandos para correr con la BD. Falta probar errores

--- programs/inicio.php
+++ programs/inicio.php
@@ -14,13 +14,28 @@ Falta conexión con BD y el contenido que se desplegará-->
 		if(isset($_POST['usuario']) && isset($_POST['contra']))
 		{
 			//insertar la query
-			if($_POST['usuario']=='315294378')		//existe el usuario en la DB
+			$conexion=mysqli_connect("127.0.0.1","root","","PROFIN");
+			$usuario=mysqli_real_escape_string($conexion,$_POST['usuario']);
+			$query='SELECT * FROM USUARIOS WHERE US_NUM="'.$usuario.'";';
+			$result=mysqli_query($conexion,$query);
+			
+			if(mysqli_num_rows($result)>0)
+			//if($_POST['usuario']=='315294378')		//existe el usuario en la DB
 			{
 				echo 'usuario bien';
-				if($_POST['contra']=='hola')
-				{
-					echo 'Todo bien';
-					if($tipo=='J')
+				$resultArray=mysqli_fetch_assoc($result);
+				$contra=mysqli_real_escape_string($conexion,$_POST['contra']);	//implementar hash?
+				if($contra==$resultArray['USUARIO_HSP'])
+				//if($_POST['contra']=='hola')
+				{	
+					echo 'Todo bien';					//IDEA: Aquí creas las variables de sesión y más abajo pones lo demás
+					$tipo=$resultArray['US_TIPO'];
+					session_start();
+					$_SESSION['nombre']=$resultArray['US_NOMBRE'];
+					$_SESSION['tipo']=$tipo;
+					$_SESSION['usuario']=$resultArray['US_NUM'];
+					
+					/*if($tipo=='J')						//esto se comentaria
 					{
 						jugador();	//despliega pantalla para el alumno
 					}
@@ -35,13 +50,36 @@ Falta conexión con BD y el contenido que se desplegará-->
 					else if($tipo=='A')
 					{
 						administrador();	//despliega pantalla para el administrador
-					}
+					}*/
 				}
 				else
 					echo '<p>Contraseña incorrecta</p><a href="principal.html">Página Principal</a>';
 			}
 			else
 				echo '</p>Ese usuario no existe</p><a href="principal.html">Página Principal</a>';
+			
+			mysqli_close($conexion);
+		}
+		/*else
+			echo '<p>Inicia sesión</p><a href="principal.html">Página Principal</a>';	//esto se comenta*/
+		if(isset($_SESSION['nombre']) && isset($_SESSION['tipo']) && isset($_SESSION['usuario']))
+		{
+			if($_SESSION['TIPO']=='J')						
+			{
+				jugador();	//despliega pantalla para el alumno
+			}
+			else if($tipo=='P')
+			{
+				profesor();	//despliega pantalla para el profesor
+			}
+			else if($tipo=='C')
+			{
+				coordinador();	//despliega pantalla para el coordinador
+			}
+			else if($tipo=='A')
+			{
+				administrador();	//despliega pantalla para el administrador
+			}
 		}
 		else
 			echo '<p>Inicia sesión</p><a href="principal.html">Página Principal</a>';
