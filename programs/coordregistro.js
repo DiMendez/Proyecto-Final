b3211b9540bdd127a8dfd4be07cb03820f4eec93
Alma Sánchez <Alma Snchez>
2016-05-26 01:36:16 -0500

    Ya está lo de registro de coordinadores, solo falta lo del nombre por la longitud

---
+++ programs/coordregistro.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	$("#ok").hide();
+		$("#form").validate({
+        rules: {
+            nom: { required: true,lettersonly: true, minlength:4, maxlength:20},
+            app: { required: true,lettersonly:true, minlength: 4, maxlength:20},
+			apm:{ required: true, lettersonly:true,minlength: 4, maxlength:20},
+			cuenta:{required:true, minlength:6, maxlength:6,digits:true},
+        },
+        messages: {
+            nom: "Debe introducir su nombre con mínimo cuatro letras, sin acentos o caracteres especiales.",
+            app: "Debe introducir apellido con mínimo cuatro letras, sin acentos o caracteres especiales.",
+			apm:"Debe introducir apellido válido",
+			cuenta:"Debe de ser una cuenta con seis números",
+        },
+        submitHandler: function(form){
+            var dataString = 'nom='+$('#nom').val()+'&cuenta='+$('#cuenta').val()+'&app='+$('#app').val()+'&apm='+$('#apm').val();
+            $.ajax({
+                type: "POST",
+                url:"../programs/coordregistro.php",
+                data: dataString,
+                success: function(data){
+                    $("#ok").html(data);
+                    $("#ok").show();
+                    $("#form").hide();
+                }
+            });
+			alert("El formulario se ha enviado");
+        }
+    });
+	
+});
