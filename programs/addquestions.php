b4df973b791b558a69c904723f9f9e9315c84104
Alma Sánchez <Alma Snchez>
2016-05-27 22:13:37 -0500

    Ya sube media y todo 2

--- programs/addquestions.php
+++ programs/addquestions.php
@@ -1,23 +1,38 @@
-<?php
-if(isset($_POST['materia']))
-{
-	$conexion=mysqli_connect("127.0.0.1","root","","PROFIN");
-	$query='SELECT UNIDAD_NO,UNIDAD_NOMBRE FROM UNIDAD WHERE MATERIA_NO="'.$_POST['materia'].'";';
-	$result=mysqli_query($conexion,$query);
-	$html='<legend>Escoge Unidad</legend>';
-	for($x=0;$x<mysqli_num_rows($result);$x++)
-	{
-		$y=$x+1;
-		$resultArray=mysqli_fetch_assoc($result);
-		$numero=$resultArray["UNIDAD_NO"];
-		$nombre=$resultArray["UNIDAD_NOMBRE"];
-		$nuevo='<div class="radio"><label>'.$numero.'.- '.$nombre.'
-														<input type="radio" name="unidad" value="'.$numero.'"/>
-													</label>
-												</div>';
-		$html=$html.$nuevo;
-	}
-	mysqli_close($conexion);
-	echo $html;
-}
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Nombre</title>		
+		<script src="../programs/jquery-2.2.3.js"></script>
+		<script src="../frameworks/bootstrap-3.3.6-dist/js/bootstrap.js"></script>
+		<link rel="stylesheet" href="../frameworks/bootstrap-3.3.6-dist/css/bootstrap.css"/>
+		<link rel="stylesheet" href="../styles/board.css"/>
+	</head>
+	<body>
+		<?php
+		if(isset($_POST['materia']))
+		{
+			$conexion=mysqli_connect("127.0.0.1","root","","PROFIN");
+			$query='SELECT UNIDAD_NO,UNIDAD_NOMBRE FROM UNIDAD WHERE MATERIA_NO="'.$_POST['materia'].'";';
+			$result=mysqli_query($conexion,$query);
+			$html='<legend>Escoge Unidad</legend>';
+			for($x=0;$x<mysqli_num_rows($result);$x++)
+			{
+				$y=$x+1;
+				$resultArray=mysqli_fetch_assoc($result);
+				$numero=$resultArray["UNIDAD_NO"];
+				$nombre=$resultArray["UNIDAD_NOMBRE"];
+				$nuevo='<div class="radio"><label>'.$numero.'.- '.$nombre.'
+																<input type="radio" name="unidad" value="'.$numero.'"/>
+															</label>
+														</div>';
+				$html=$html.$nuevo;
+			}
+			mysqli_close($conexion);
+			echo $html;
+		}
+		?>
+	</body>
+</html>
--- programs/preguntas.php
+++ programs/preguntas.php
@@ -1,97 +1,66 @@
-<?php
-	$conexion=mysqli_connect("127.0.0.1","root","","PROFIN"); //Establece conexión con la base de datos
-	if(isset ($_POST['materia']))
-	{
-		$materia=$_POST['materia'];
-		if(isset($_POST['unidad']))
-		{
-			$unidad=$_POST['unidad'];
-			if( isset($_POST['reactivo'])&& isset($_POST['uno']) && isset($_POST['dos']) && isset($_POST['tres']) && isset($_POST['cuatro']))
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Nombre</title>		
+		<script src="../programs/jquery-2.2.3.js"></script>
+		<script src="../frameworks/bootstrap-3.3.6-dist/js/bootstrap.js"></script>
+		<link rel="stylesheet" href="../frameworks/bootstrap-3.3.6-dist/css/bootstrap.css"/>
+		<link rel="stylesheet" href="../styles/board.css"/>
+	</head>
+	<body>
+		<?php
+			$conexion=mysqli_connect("127.0.0.1","root","","PROFIN"); //Establece conexión con la base de datos
+			if(isset ($_POST['materia']))
 			{
-				$reactivo=mysqli_real_escape_string($conexion,$_POST['reactivo']); //recibe cadena de la pregunta a agregar
-				$opuno=mysqli_real_escape_string($conexion,$_POST['uno']); //Esto recibe la cadena de las opciones
-				$opdos=mysqli_real_escape_string($conexion,$_POST['dos']); //Está protegido contra inyecciones sql
-				$optres=mysqli_real_escape_string($conexion,$_POST['tres']);
-				$opcuatro=mysqli_real_escape_string($conexion,$_POST['cuatro']);
-				$correcta=mysqli_real_escape_string($conexion,$_POST['correcta']);
-				/*if(isset($_FILES['media'])) //Si existe la selección del archivo
+				$materia=$_POST['materia'];
+				if(isset($_POST['unidad']))
 				{
-					var_dump($_FILES['media']['name']);
-					$nomarchivo=$_FILES['media']['name']; //guarda nombre del archivo
-					$tipo=$_FILES['media']['type']; //Tipo del archivo
-					$size=$_FILES['media']['size']; //Size del archivo
-					//vamos a comprobar si el archivo que sube usuario es válido
-					//si no encuentran una coincidencia con eso, entonces es incorrecto
-					if(!((strpos($tipo,"gif") || strpos($tipo,"png") 
-						|| strpos($tipo,"jpeg") || strpos($tipo,"bmp")) && ($size <200000)))
+					$unidad=$_POST['unidad'];
+					if( isset($_POST['reactivo'])&& isset($_POST['uno']) && isset($_POST['dos']) && isset($_POST['tres']) && isset($_POST['cuatro']))
 					{
-						echo'El tamaño o la extensión no es correcta';
-						echo'<br/>';
-						echo'Se permiten archivos con extensión gif,jpeg,png,bmp y un tamaño menor a 200 kb';
-						echo'<a href="../inicio.php">Regresar</a>';
-					}
-					else
-					{
-						//vamos a mandar la información a la DB
-						$file=$_FILES['media']['tmp_name'];
-						if(move_uploaded_file($file,'../resources/'.$nomarchivo)) //si se guarda en Resources...
+						$reactivo=mysqli_real_escape_string($conexion,$_POST['reactivo']); //recibe cadena de la pregunta a agregar
+						$opuno=mysqli_real_escape_string($conexion,$_POST['uno']); //Esto recibe la cadena de las opciones
+						$opdos=mysqli_real_escape_string($conexion,$_POST['dos']); //Está protegido contra inyecciones sql
+						$optres=mysqli_real_escape_string($conexion,$_POST['tres']);
+						$opcuatro=mysqli_real_escape_string($conexion,$_POST['cuatro']);
+						$correcta=mysqli_real_escape_string($conexion,$_POST['correcta']);
+						if($conexion) //si no hay archivos y si hay conexión, entonces inserta a base de datos
 						{
-							if($conexion) //verifica que la conexión esté lista
+							
+							$cad='INSERT INTO PREGUNTAS VALUES("'.$materia.'",'.$unidad.',"'.$reactivo.'","",
+								"'.$opuno.'","'.$opdos.'","'.$optres.'","'.$opcuatro.'","'.$correcta.'","NO")';
+							$ruta=mysqli_query($conexion,$cad);
+							if($ruta)
 							{
-								$cad='INSERT INTO PREGUNTAS VALUES("'.$materia.'",'.$unidad.',"'.$reactivo.'","../resources/'.$nomarchivo.'",
-									"'.$opuno.'","'.$opdos.'","'.$optres.'","'.$opcuatro.'","'.$correcta.'","NO")';
-								$ruta=mysqli_query($conexion,$cad);
-								if($ruta)
-								{
-									echo'Tu pregunta se ha cargado correctamente';
-								}
-								
-							}
+								echo'Tu pregunta se ha cargado correctamente';
+								echo'<form method="POST" action="preguntas_media.php" enctype="multipart/form-data">
+									<label><legend>Añadir archivos (recomendado para imágenes)</legend></label>
+									<input type="file" name="media" value="Upload Image"/>
+									<input type="submit" name="Enviar"/>
+								</form>';
+							}	
 							else 
 							{
 								echo'Al parecer, hubo un error';
 								echo'<a href="../inicio.php">Regresar</a>';
+								echo 'La pregunta se ha enviado exitosamente';
 							}
 						}
-						else
-						{
-							echo'Al parecer hubo un error al cargar archivo';
-							echo'<a href="../inicio.php">Regresar</a>';
-						}
 					}
-				}
-				else*/if($conexion) //si no hay archivos y si hay conexión, entonces inserta a base de datos
-				{
-					
-					$cad='INSERT INTO PREGUNTAS VALUES("'.$materia.'",'.$unidad.',"'.$reactivo.'","",
-						"'.$opuno.'","'.$opdos.'","'.$optres.'","'.$opcuatro.'","'.$correcta.'","NO")';
-					$ruta=mysqli_query($conexion,$cad);
-					if($ruta)
-					{
-						echo'Tu pregunta se ha cargado correctamente';
-						echo'<form method="POST" action="preguntas_media.php" enctype="multipart/form-data">
-							<label><legend>Añadir archivos (recomendado para imágenes)</legend></label>
-							<input type="file" name="media" value="Upload Image"/>
-							<input type="submit" name="Enviar"/>
-						</form>';
-					}	
 					else 
 					{
-						echo'Al parecer, hubo un error';
+						echo'Es necesario que inserte cuatro opciones';
 						echo'<a href="../inicio.php">Regresar</a>';
-						echo 'La pregunta se ha enviado exitosamente';
 					}
 				}
+				else
+					echo'Necesita escoger una unidad';
 			}
-			else 
-			{
-				echo'Es necesario que inserte cuatro opciones';
-				echo'<a href="../inicio.php">Regresar</a>';
-			}
-		}
-		else
-			echo'Necesita escoger una unidad';
-	}
-	else
-		echo'Necesita escoger una materia';
-?>
+			else
+				echo'Necesita escoger una materia';
+		?>
+	</body>
+</html>
